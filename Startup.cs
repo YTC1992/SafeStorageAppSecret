6bd06d8cff7b808dddc0a75d3a2b45689e9b303b
YT <wendy199288@gmail.com>
2022-08-15 15:21:46 +0800

    新增引用 System.Data.SqlClient 於 startup.cs

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
